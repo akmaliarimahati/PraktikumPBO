b41189f67b266d659cdea2490b247a9646cb3fed
akmaliarimahati <akmaliarimahati09@gmail.com>
2026-09-11 17:12:17 +0700

    percobaan 3

---
+++ Jobsheet03/DemoUser.java
@@ -0,0 +1,8 @@
+package Jobsheet03;
+
+public class DemoUser {
+    public static void main(String[] args) {
+        User user1 = new User("akmalia.rima", "Akmalia Rima Hati", "akmalia.rima@gmail.com");
+        user1.cetakInfo();
+    }
+}
